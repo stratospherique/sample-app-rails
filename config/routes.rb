624c92daff2191ab710212154cb86b529474818d
stratospheriquegit config --global user.name stratospheriquegit config --global user.name stratospherique <ahmedosm10@gmail.com>
2019-05-09 00:09:51 +0100

    finish static pages

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,8 @@ Rails.application.routes.draw do
 
   get 'static_pages/about'
 
+  get 'static_pages/contact'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root 'application#hello'
+  root 'static_pages#home'
 end
