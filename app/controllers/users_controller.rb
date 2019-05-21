48d0984bef4a1b72aa0e323aa8eda74ee264117b
stratospheriquegit config --global user.name stratospheriquegit config --global user.name stratospherique <ahmedosm10@gmail.com>
2019-05-21 20:26:23 +0100

    finish sign up views and actions

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,23 @@
 class UsersController < ApplicationController
   def new
+    @user = User.new
   end
 
   def show
     @user = User.find(params[:id])
+  end
+
+  def create
+    @user = User.new(creating_params)
+    if @user.save
+      redirect_to user_path(@user)
+    else
+      render 'new'
+    end
+  end
+
+  private
+  def creating_params
+    params.require(:user).permit(:name,:email,:password,:password_confirmation)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -9,7 +9,7 @@ Rails.application.routes.draw do
 
   get '/contact',to:'static_pages#contact'
 
-  resources :users , only:(:show)
+  resources :users , only:([:show,:new,:create])
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   
