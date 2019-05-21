125e8ecb39999feec7bb9123a610042f7b2a8aa3
stratospheriquegit config --global user.name stratospheriquegit config --global user.name stratospherique <ahmedosm10@gmail.com>
2019-05-21 14:21:10 +0100

    add a view for displaying users with an avatar generated using MD5 method on downcased email

--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,8 @@ Rails.application.routes.draw do
 
   get '/contact',to:'static_pages#contact'
 
+  resources :users , only:(:show)
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   
 end
