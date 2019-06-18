2f981cf9c2b5cabf7e3648fcb61f21eb176d9f3f
stratospheriquegit config --global user.name stratospheriquegit config --global user.name stratospherique <ahmedosm10@gmail.com>
2019-06-18 19:32:57 +0100

    implement password reset on dev env

--- app/models/user.rb
+++ app/models/user.rb
@@ -58,8 +58,7 @@ class User < ApplicationRecord
    # Sets the password reset attributes.
    def create_reset_digest
     self.reset_token = User.new_token
-    update_attribute(:reset_digest,  User.digest(reset_token))
-    update_attribute(:reset_sent_at, Time.zone.now)
+    update_columns(:reset_digest => User.digest(reset_token), :reset_sent_at => Time.zone.now)
   end
 
   # Sends password reset email.
