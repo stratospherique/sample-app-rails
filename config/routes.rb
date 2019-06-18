fded00780a265183979d2e37706c545f37413748
stratospheriquegit config --global user.name stratospheriquegit config --global user.name stratospherique <ahmedosm10@gmail.com>
2019-06-18 19:13:57 +0100

    password reset without testing

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
   
 
+  get 'password_resets/new'
+
+  get 'password_resets/edit'
+
   resources :users , only: [:create,:edit,:update,:show,:destroy]
 
   get '/users', to: 'users#index'
@@ -26,6 +30,7 @@ Rails.application.routes.draw do
   resources :account_activations, only: :edit
   
 
+  resources :password_resets,     only: [:new, :create, :edit, :update]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   
 end
